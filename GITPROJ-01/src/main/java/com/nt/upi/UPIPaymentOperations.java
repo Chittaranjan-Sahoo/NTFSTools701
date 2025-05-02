ad8ed5a19e2243cde617ca1c0650baf1c297c0fb
Chittaranjan-Sahoo <sahoochittaranjan980@gmail.com>
2025-05-02 16:06:48 +0530

    TL Reviewed Branch-1 code

--- GITPROJ-01/src/main/java/com/nt/upi/UPIPaymentOperations.java
+++ GITPROJ-01/src/main/java/com/nt/upi/UPIPaymentOperations.java
@@ -2,9 +2,10 @@ package com.nt.upi;
 
 public class UPIPaymentOperations {
 	
+	//performs the upi payment
 	public String doPayement(long phno,double amt)
 	{
-		return amt+ "amount is paid to phone number"+phno;
+		return amt+ "amount is paid to @phone number"+phno;
 	}
 
 }
